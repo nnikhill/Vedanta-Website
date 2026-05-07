bc0514497f98e784d57d03d9d62e8f4170906e8d
nnikhill <nnikhill2004gmail.com>
2026-05-07 16:40:50 +0530

    fix messages api

--- frontend/src/Components/admin/AddCourse.jsx
+++ frontend/src/Components/admin/AddCourse.jsx
@@ -9,7 +9,7 @@ export default function AddCourse() {
     description: "",
   });
 
-  const token = localStorage.getItem("token");
+  const token = sessionStorage.getItem("token");
   const API = "https://vedanta-website.onrender.com/api";
 
   const handleChange = (e) => {
